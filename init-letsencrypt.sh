4ed5d6a4cd3845234432cd7c7a63354ae1c78373
r4bbitz <56757818+r4bbitz@users.noreply.github.com>
2023-10-12 00:12:44 +0700

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
-  exit 1
-fi
 
-domains=(example.org www.example.org)
+domains=(service.littlejoy.cloud)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
